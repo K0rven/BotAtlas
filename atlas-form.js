06f49290ff97aebbda5502be2dccfd1ea0442152
DanielG2602 <daniel.comercial0981@gmail.com>
2026-06-27 15:24:19 -0300

    feat: dev test

--- atlas-form.js
+++ atlas-form.js
@@ -6,7 +6,7 @@
 // liga essa lógica aos elementos da página.
 
 export const N8N_WEBHOOK_URL =
-  'http://localhost:5678/webhook-test/formulario-atlas';
+  'http://localhost:5678/webhook/formulario-atlas';
 
 const VALID_SEGMENTS = [
   'clinica',
